320dc6d9944166773f3a952414b34f7bb2721aaf
LAPTOP-BF18BV75\talenthrm <haqisaurus@yahoo.com>
2021-08-31 12:37:15 +0700

    - update job form

--- src/pages/job/JobRoute.tsx
+++ src/pages/job/JobRoute.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
 import JobDetail from "./job-detail/JobDetail";
+import JobForm from "./job-form/JobForm";
 import Job from "./job/Job";
 
 export interface JobRouteProps {}
@@ -14,6 +15,7 @@ class JobRoute extends React.Component<JobRouteProps, JobRouteState> {
                 <Switch>
                     <Route exact path="/job" component={Job} />
                     <Route exact path="/job/job-detail" component={JobDetail} />
+                    <Route exact path="/job/apply" component={JobForm} />
                 </Switch>
             </Router>
         );
---
+++ src/pages/job/job-form/JobForm.tsx
@@ -0,0 +1,137 @@
+import { faArrowLeft, faChevronRight, faImages, faMapMarkerAlt, faPaperclip } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Breadcrumb, Card, Col, List, Row, Form, Typography, Input, Select, Button, Space, Divider } from "antd";
+import Avatar from "antd/lib/avatar/avatar";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export interface JobFormProps {}
+
+export interface JobFormState {}
+
+class JobForm extends React.Component<JobFormProps, JobFormState> {
+    render() {
+        return (
+            <div className="page-job-form">
+                <Row>
+                    <Col>
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <Link to="/">
+                                    <FontAwesomeIcon icon={faArrowLeft} />
+                                </Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/">Lowongan Pekerjaan</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/">Deskripsi Pekerjaan</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>Lamar</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: 15 }}>
+                    <Col span={24}>
+                        <div className="bg-header">
+                            <Row align="middle">
+                                <Col span={6} style={{ textAlign: "center", paddingTop: 50, paddingBottom: 50 }}>
+                                    <Avatar size={90} />
+                                </Col>
+                                <Col span={18}>
+                                    <Typography.Title level={5}>Lorem Boutique</Typography.Title>
+                                    <div>
+                                        <Typography.Text>Loren ipsum</Typography.Text>
+                                    </div>
+                                    <Typography.Text>
+                                        <FontAwesomeIcon icon={faMapMarkerAlt} style={{ marginRight: 5 }} />
+                                        Jakarta, Indonesia
+                                    </Typography.Text>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+                <Row gutter={[20, 20]} style={{ marginTop: 15 }}>
+                    <Col span={8}>
+                        <Typography.Title level={5}>See more another job by</Typography.Title>
+                        <List
+                            dataSource={[
+                                "Racing car sprays burning fuel into crowd.",
+                                "Japanese princess to wed commoner.",
+                                "Australian walks 100km after outback crash.",
+                                "Man charged over missing wedding girl.",
+                                "Los Angeles battles huge wildfires.",
+                            ]}
+                            split={false}
+                            bordered={false}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    <Card style={{ width: "100%", borderRadius: 20 }} bodyStyle={{ borderRadius: 20 }}>
+                                        <Row align="middle" gutter={20}>
+                                            <Col span={8} style={{ textAlign: "center" }}>
+                                                <Avatar size={64} />
+                                            </Col>
+                                            <Col span={14}>
+                                                <Typography.Title level={5}>Lorem Boutique</Typography.Title>
+                                                <div>
+                                                    <Typography.Text>Loren ipsum</Typography.Text>
+                                                </div>
+                                                <Typography.Text>
+                                                    <FontAwesomeIcon icon={faMapMarkerAlt} style={{ marginRight: 5 }} />
+                                                    Jakarta, Indonesia
+                                                </Typography.Text>
+                                            </Col>
+                                            <Col span={2}>
+                                                <FontAwesomeIcon icon={faChevronRight} />
+                                            </Col>
+                                        </Row>
+                                    </Card>
+                                </List.Item>
+                            )}
+                        />
+                    </Col>
+                    <Col span={16}>
+                        <Card style={{ width: "100%", borderRadius: 20 }} bodyStyle={{ borderRadius: 20 }}>
+                            <Form layout="vertical">
+                                <Form.Item label="Your e-mail">
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Phone Number">
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Location">
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="How many years' experience do you have as a Fashion designer?">
+                                    <Select>
+                                        <Select.Option value="demo">Demo</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item label="Describe yourself">
+                                    <Input.TextArea rows={5} />
+                                </Form.Item>
+                                <Form.Item label="Upload Files">
+                                    <Space>
+                                        <Button>
+                                            <FontAwesomeIcon icon={faImages} />
+                                        </Button>
+                                        <Button>
+                                            <FontAwesomeIcon icon={faPaperclip} />
+                                        </Button>
+                                    </Space>
+                                </Form.Item>
+                                <Divider />
+                                <Form.Item style={{ textAlign: "right" }}>
+                                    <Button type="primary">Submit</Button>
+                                </Form.Item>
+                            </Form>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default JobForm;
