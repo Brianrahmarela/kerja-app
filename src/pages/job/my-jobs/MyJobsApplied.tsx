a1d5107ab606630f60ac0b75956e410fc64959d8
Brianrahmarela <brian.rahmarela@gmail.com>
2021-09-24 11:02:50 +0700

    add list applied

--- src/pages/job/my-jobs/MyJobsApplied.tsx
+++ src/pages/job/my-jobs/MyJobsApplied.tsx
@@ -1,6 +1,6 @@
-import { faSearch, } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Tabs, Col, Input, Row, Typography, Divider, Button } from "antd";
+import { Tabs, Col, Input, Row, Typography, Divider, Button, Card, List, Spin, Space, Avatar, Badge, } from "antd";
 import { AxiosResponse } from "axios";
 import moment from "moment";
 import React from "react";
@@ -12,6 +12,9 @@ import { getSearchJob } from "../../../repository/JobRepo";
 import SvgTime from "../../../assets/svg/time.svg";
 import SvgNotifJobAlert from "../../../assets/svg/notif-job-alert-settings.svg";
 import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroller";
+import SvgLainnya from "../../../assets/svg/lainnya-icon.svg";
+import AvaApplied from "../../../assets/image/avatar-applied.png";
 
 
 const { Search } = Input;
@@ -26,8 +29,7 @@ export interface JobState {
   hasMore: boolean;
   loading: boolean;
   scrolled: boolean;
-  jobs: any[];
-  jobsRecomendation: any[];
+  allJobs: any[];
   pagination: any;
   valSearch: any;
 }
@@ -37,89 +39,80 @@ class MyJobsApplied extends React.Component<JobProps, JobState> {
     hasMore: true,
     loading: false,
     scrolled: false,
-    jobs: [
+    allJobs: [
       {
         "id": 1,
         "Jobtitle": "Fashion Designer",
         "company": "Lorem Ipsum",
-        "appliedon": "45 minutes ago",
+        "applicant": "123 Applicant",
         "position": "Full time",
-        "status": "Interview",
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "Applied",
       },
       {
         "id": 2,
-        "Jobtitle": "Merchandiser",
-        "company": "Colin Fashion",
-        "appliedon": "10/ 08/ 21",
+        "Jobtitle": "Fashion Designer",
+        "company": "Lorem Ipsum",
+        "applicant": "123 Applicant",
         "position": "Full time",
-        "status": "On Process",
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "Applied",
+
       },
       {
         "id": 3,
-        "Jobtitle": "Fashion Pattern",
-        "company": "Salvadore Salie",
-        "appliedon": "10/ 08/ 21",
+        "Jobtitle": "Fashion Designer",
+        "company": "Lorem Ipsum",
+        "applicant": "123 Applicant",
         "position": "Full time",
-        "status": "On Process",
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "Interview",
+
       },
       {
         "id": 4,
-        "Jobtitle": "Creative Designer",
-        "company": "Hijabers Fashion Ind ...",
-        "appliedon": "10/ 08/ 21",
+        "Jobtitle": "Fashion Designer",
+        "company": "Lorem Ipsum",
+        "applicant": "123 Applicant",
         "position": "Full time",
-        "status": "On Process",
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "On Hold",
+
       },
       {
         "id": 5,
         "Jobtitle": "Fashion Designer",
         "company": "Lorem Ipsum",
-        "appliedon": "10/ 08/ 21",
+        "applicant": "123 Applicant",
         "position": "Full time",
-        "status": "On Process",
-      }
-    ] as any[],
-    jobsRecomendation: [
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "Rejected",
+
+      },
       {
-        "id": 1,
+        "id": 6,
         "Jobtitle": "Fashion Designer",
-        "name": "Salvadore Salie",
-        "place": "DKI Jakarta",
-        "status": "Penuh Waktu / Kontrak",
-        "gaji": "Rp 3.000.000 - Rp 5.000.000",
-        "time": "2 h",
-      },
-      {
-        "id": 2,
-        "Jobtitle": "Fashion Tes",
-        "name": "Saliem",
-        "place": "Depok",
-        "status": "Magang",
-        "gaji": "Rp 1.000.000 - Rp 2.000.000",
-        "time": "3 days ago",
-
-      },
-      {
-        "id": 3,
-        "Jobtitle": "Developer",
-        "name": "No 3",
-        "place": "Papua",
-        "status": "Magang",
-        "gaji": "Rp 5000.000 - Rp 1.000.000",
-        "time": "2 days ago",
-
-      },
-      {
-        "id": 4,
-        "Jobtitle": "Tes 4",
-        "name": "no 4",
-        "place": "Depok",
-        "status": "Kontrak",
-        "gaji": "Rp 9.000.000 - Rp 2.000.000",
-        "time": "0 days ago",
+        "company": "Lorem Ipsum",
+        "applicant": "123 Applicant",
+        "position": "Full time",
+        "appliedOn": "45 minutes ago",
+        "postedOn": "5 hours ago",
+        "salary": "Rp. 3.000.000 - Rp. 5.000.000",
+        "status": "Rejected",
 
       }
     ] as any[],
+
     pagination: {
       page: 1,
       total: 0,
@@ -143,7 +136,7 @@ class MyJobsApplied extends React.Component<JobProps, JobState> {
   }
   loadMore = (e: any) => {
     // const { pagination, jobs, jobsRecomendation } = this.state;
-    const { pagination, jobs, } = this.state;
+    const { pagination, allJobs, } = this.state;
     this.setState({
       loading: true,
     });
@@ -157,10 +150,10 @@ class MyJobsApplied extends React.Component<JobProps, JobState> {
 
     getSearchJob(pagination)
       .then((res: AxiosResponse<any>) => {
-        let newPostingList: any[] = res.data.content?.concat(jobs) as any[];
+        let newPostingList: any[] = res.data.content?.concat(allJobs) as any[];
         newPostingList = newPostingList.sort((a: any, b: any) => moment(b.createdAt).diff(moment(a.createdAt)));
         this.setState({
-          jobs: newPostingList,
+          allJobs: newPostingList,
           hasMore: newPostingList.length < res.data.total,
           pagination: {
             ...pagination,
@@ -180,11 +173,11 @@ class MyJobsApplied extends React.Component<JobProps, JobState> {
   };
   render() {
     // const { currentUser } = this.props;
-    // const { jobs } = this.state;
+    const { allJobs } = this.state;
     // const { jobsRecomendation } = this.state;
     return (
       <div className="job-page">
-        <Row style={{ marginTop: 64 }} justify="space-around" align="middle">
+        <Row style={{ marginTop: 64, marginBottom: 25.5 }} justify="space-around" align="middle">
           <Col xs={24} md={13}>
             <Row align="middle" style={{ marginBottom: 10 }}>
 
@@ -208,61 +201,153 @@ class MyJobsApplied extends React.Component<JobProps, JobState> {
         </Row>
         <Divider style={{ margin: '12px 0px', padding: 0, }} />
 
-        <Row justify="space-between">
-          <Col style={{ backgroundColor: "honeydew" }}>
-            <Tabs defaultActiveKey="2" onChange={this.callback} tabBarExtraContent={
-              <Link to={`/home`} >
+        <Row style={{ backgroundColor: "honeydew" }}>
+          {/* <Col > */}
+          <Tabs defaultActiveKey="2" onChange={this.callback} tabBarExtraContent={
+            <Link to={`/home`} >
 
-                <Button type="link" block style={{ padding: 0, margin: 0, }}><img
-                  src={SvgNotifJobAlert}
-                  alt="jobalertsettings"
-                  height={16}
-                  style={{ marginRight: 9, }}
-                />
-                  Job Alert Settings</Button>
-              </Link>
-            }>
-              <TabPane tab="All" key="1">
-                All
-              </TabPane>
-              <TabPane tab="Applied" key="2">
-                <Row style={{ marginTop: 22, backgroundColor: "yellow" }} align="middle">
-                  <Col style={{ marginRight: 9 }}>
+              <Button type="link" block style={{ padding: 0, margin: 0, }}><img
+                src={SvgNotifJobAlert}
+                alt="jobalertsettings"
+                height={16}
+                style={{ marginRight: 9, }}
+              />
+                Job Alert Settings</Button>
+            </Link>
+          }
+            tabBarGutter={24}
+          >
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">All</Button>} key="1" >
+              All content
+            </TabPane>
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">Applied</Button>} key="2">
+              <Row style={{ marginTop: 22, backgroundColor: "yellow" }} align="middle">
+                <Col style={{ marginRight: 9 }}>
+                  <img style={{ padding: 0, margin: 0, }}
+                    src={SvgTime}
+                    alt="myLastappliedjob"
+                    height={22}
+                  />
+                </Col>
+                <Col>
+                  <Text className="subtitlejob">Applied Job</Text>
+                </Col>
+              </Row>
+              <InfiniteScroll initialLoad={true} pageStart={0} loadMore={this.loadMore} hasMore={!this.state.loading && this.state.hasMore} useWindow={true} className="mobilehiddenmylast">
+                <List
+                  dataSource={allJobs || []}
+                  split={false}
+                  locale={{
+                    emptyText: <Card>No Post</Card>,
+                  }}
+                  renderItem={(job: any, i: number) => (
+                    <div>
+                      {/* `{console.log(job)}` */}
+                      <List.Item key={job.id} style={{ padding: 0, marginBottom: 15, width: "100%", }}>
+                        <Card style={{ width: '100%', borderRadius: 8 }} bordered={true} className="cardApplied">
+
+                          <Row justify="space-between">
+                            {/* <Space size={50}> */}
+
+                            <Col span={5} style={{ backgroundColor: "green", }}>
+
+                              <span className="avatar-item">
+                                <Badge count={1} className="badgeApplied">
+                                  <Avatar shape="square" src={AvaApplied} size={131} />
+                                </Badge>
+                              </span>
+                            </Col>
+                            <Col span={1}></Col>
+                            <Col span={18} style={{ backgroundColor: "yellow" }}>
+                              <Row justify="space-between">
+                                <Col>
+                                  <Row>
+                                    <Col>
+                                      <p>{job.Jobtitle}</p>
+                                    </Col>
+                                    <Col  >
+                                      <p>{job.company}</p>
+                                    </Col>
+                                    <Col  >
+                                      <p>{job.applicant}</p>
+                                    </Col>
+                                    <Col  >
+                                      <p>{job.position}</p>
+                                    </Col>
+                                  </Row>
+                                </Col>
+                                <Col>Quick View</Col>
+                              </Row>
+                              <Divider style={{ margin: '2px 0px', padding: 0, }} />
+                              <Row>
+                                <Col>
+                                  Salary: <p>{job.salary}</p>
+                                </Col>
+                                <Col>
+                                  {job.status === "Interview" ? (
+                                    <Row align="middle">
+                                      <Col style={{ marginRight: 3 }}>
+                                        <p className="listtitleMobile">Status : </p>
+                                      </Col>
+                                      <Col>
+                                        <Button type="default" className="btnmobilemylast" style={{ backgroundColor: "green", color: "white", border: '0px' }} >{job.status}</Button>
+                                      </Col>
+                                    </Row>
+                                  ) : (
+                                    <Row align="middle">
+                                      <Col style={{ marginRight: 3 }}>
+                                        <p className="listtitleMobile">Status: </p>
+                                      </Col>
+                                      <Col>
+                                        <Button type="primary" className="btnmobilemylast" >{job.status}</Button>
+                                      </Col>
+                                    </Row>
+                                  )
+                                  }
+                                </Col>
+                              </Row>
+
+                            </Col>
+                            {/* </Space> */}
+                          </Row>
+                        </Card>
+                      </List.Item>
+                    </div>
+                  )}
+                >
+                  {this.state.loading && this.state.hasMore && <Spin indicator={<FontAwesomeIcon icon={faCircleNotch} className="fa-spin" />} />}
+                </List>
+                <Button type="link" block style={{ marginBottom: 49, marginTop: 22 }}>
+                  <Space size={9} style={{ color: "#53575E", }}>
+                    Lainnya
                     <img style={{ padding: 0, margin: 0, }}
-                      src={SvgTime}
-                      alt="myLastappliedjob"
-                      height={22}
+                      src={SvgLainnya}
+                      alt="share"
+                      height={5}
                     />
-                  </Col>
-                  <Col>
-                    <Text className="subtitlejob">Applied Job</Text>
-                  </Col>
-                </Row>
-                AppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedAppliedApplied
-              </TabPane>
-              <TabPane tab="On-Progress" key="3">
-                On-Progress
-              </TabPane>
-              <TabPane tab="Accepted" key="4">
-                Accepted
-              </TabPane>
-              <TabPane tab="Closed" key="5">
-                Closed
-              </TabPane>
-              <TabPane tab="Bookmark" key="6">
-                Bookmark
-              </TabPane>
-              <TabPane tab="xx" key="7">
+                  </Space>
+                </Button>
+              </InfiniteScroll>
+            </TabPane>
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">On-Progress</Button>} key="3">
+              On-Progress Content
+            </TabPane>
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">Accepted</Button>} key="4">
+              Accepted Content
+            </TabPane>
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">Closed</Button>} key="5">
+              Closed Content
+            </TabPane>
+            <TabPane tab={<Button type="ghost" className="btntabMyJob">Bookmark</Button>} key="6">
+              Bookmark Content
+            </TabPane>
 
-
-              </TabPane>
-            </Tabs>
-
-
-          </Col>
+          </Tabs>
 
 
         </Row>
+
+
 
 
 
